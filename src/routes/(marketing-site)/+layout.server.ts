63727b994ead29386baab27ba2ce0dec50c7e2cd
lluke12 <luke@shoutmedia.nl>
2025-10-25 15:08:01 +0200

    Update +layout.server.ts

--- src/routes/(marketing-site)/+layout.server.ts
+++ src/routes/(marketing-site)/+layout.server.ts
@@ -1,23 +1,10 @@
-import {
-  fetchAwards,
-  fetchAwardsTypes,
-  fetchCareers,
-  fetchHomeBlogPosts,
-  fetchTeamMembers
-} from '$lib/content';
-
-export const load = async ({ locals, fetch }) => {
-  const version = locals.version;
-
+export const load = async () => {
   return {
-    // Note: WordPress doesn't have a "configuration" concept like Storyblok
-    // Site configuration would need to come from WordPress options or a specific page
     configuration: null,
-    // careers: await fetchCareers({ version, fetch }),
-    // awards: await fetchAwards({ version, fetch }),
-    // awardsTypes: await fetchAwardsTypes({ version, fetch }),
-    // teamMembers: await fetchTeamMembers({ version, fetch }),
-    // homePosts: await fetchHomeBlogPosts({ version, fetch }),
-    version
+    careers: [],
+    awards: [],
+    awardsTypes: [],
+    teamMembers: [],
+    homePosts: []
   };
 };
