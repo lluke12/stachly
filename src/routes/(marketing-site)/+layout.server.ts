57a1939f3aeb76c0358f94ebec98f45609f9c81a
lluke12 <luke@shoutmedia.nl>
2025-10-25 14:57:52 +0200

    Update +layout.server.ts

--- src/routes/(marketing-site)/+layout.server.ts
+++ src/routes/(marketing-site)/+layout.server.ts
@@ -13,7 +13,7 @@ export const load = async ({ locals, fetch }) => {
     // Note: WordPress doesn't have a "configuration" concept like Storyblok
     // Site configuration would need to come from WordPress options or a specific page
     configuration: null,
-    careers: await fetchCareers({ version, fetch }),
+    // careers: await fetchCareers({ version, fetch }),
     awards: await fetchAwards({ version, fetch }),
     awardsTypes: await fetchAwardsTypes({ version, fetch }),
     teamMembers: await fetchTeamMembers({ version, fetch }),
